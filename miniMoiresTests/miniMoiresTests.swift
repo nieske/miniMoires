a947c3dabb62f1120aba0f00ee4463bcf0e4e112
Nieske Vergunst <nieske@nieskevergunst.nl>
2015-01-04 14:09:04 +0100

    Initial Commit

---
+++ miniMoiresTests/miniMoiresTests.swift
@@ -0,0 +1,36 @@
+//
+//  miniMoiresTests.swift
+//  miniMoiresTests
+//
+//  Created by Nieske Vergunst on 04-01-15.
+//  Copyright (c) 2015 Lady Geek. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class miniMoiresTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
